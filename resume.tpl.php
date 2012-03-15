c07e8bc6419b3535f6601cf2a5670cb8fe032bd0
Albert Volkman <albert@rise.net>
2012-03-15 16:53:47 -0400

    Updates

--- resume.tpl.php
+++ resume.tpl.php
@@ -7,15 +7,15 @@
   print 'Positions of interest: ' . print_r($positions,1) . '<br />';
   print 'Available starting: ' . $startwork . '<br />';
 
-  print '<h3>Work Eligibility</h3>' . print_r($available,1) . '<br />';
+  print '<h3>Work Eligibility</h3>' . drupal_render($available) . '<br />';
 
   print '<h2>Education and Skills</h2>';
   print 'Highest level of education: ' . $education . '<br />';
-  print 'Skills and Qualifications: ' . print_r($skills,1) . '<br />';
+  print 'Skills and Qualifications: ' . $skills . '<br />';
   print 'Project Achievements: ' . print_r($achievements,1) . '<br />';
 
   print '<h2>Work History</h2>';
-  print 'Armed forces: ' . $armedforces . '<br />';
-  print 'Experience: ' . print_r($experience,1) . '<br />';
-  print 'References: ' . print_r($reference,1) . '<br />';
+  print 'Armed forces: ' . drupal_render($armedforces) . '<br />';
+  print 'Experience: ' . $experience . '<br />';
+  print 'References: ' . $reference . '<br />';
 ?>
